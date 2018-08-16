832fd24388091d9d12edc9643c69bc4284378009
Charle Lien <lienching31240@gmail.com>
2018-08-16 16:47:06 +0800

    Make fibonacci sequence work properly

--- Hw1-4/fibonacci.c
+++ Hw1-4/fibonacci.c
@@ -7,14 +7,16 @@ unsigned int* Fib;
 int main(int argc, char *argv[] )
 {
 	int i,N;
-	
+    if ( argc < 2 )
+        exit(0);
+    
 	N = atoi(argv[1]);
 
-	Fib = (unsigned int *)malloc(N);
+	Fib = (unsigned int *)malloc(N * sizeof(int));
 	fibonacci(N);
 
 	printf("The sequence of Fibonacci up to %d:\n", (N+1));
-	for (i=0; i<=N; i++); 
+	for (i=0; i<=N; i++) 
 		printf("%d, ", fibonacci(i));
 	printf("\n");
 	free(Fib);
@@ -28,5 +30,5 @@ unsigned int fibonacci( int n )
 	if ( n <= 1)
 		return (Fib[n]=1);
 	else
-		return (Fib[n]=fibonacci(n) + fibonacci(n-1));
+		return (Fib[n]=fibonacci(n-1) + fibonacci(n-2));
 }
